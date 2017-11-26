a385195a55854fbe9eef52dc592cdfd41f5ae164
Andrei Zhazhko <kfkrgf@gmail.com>
2017-11-26 14:34:31 +0200

    version1.3 svg

--- js/custom.js
+++ js/custom.js
@@ -2,14 +2,14 @@ jQuery(function($){
 $(document).ready(function() {
     
    $("button.mainMenu").click(function() {
-       if ($("div.mainMenu").hasClass("invis")) {
-           $("div.mainMenu").removeClass("invis");
+       if ($("div.mainMenu").hasClass("hidden")) {
+           $("div.mainMenu").removeClass("hidden");
        }
        
    });
     
     $("button.closeMenu").click(function() {
-        $("div.mainMenu").addClass("invis");
+        $("div.mainMenu").addClass("hidden");
     })
 });
 	
